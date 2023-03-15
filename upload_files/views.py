e0cae75cc48212d981e815a57f7b318e77265cff
KWunsch <kevin.wunsch@tu-darmstadt.de>
2023-03-15 16:15:32 +0100

    minor changes, DRY

--- upload_files/views.py
+++ upload_files/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
 from django.contrib.staticfiles.utils import get_files
@@ -10,16 +10,17 @@ def index(request):
     return render(request, "upload_files/index.html")
 
 def upload_file(request):
+    '''displays upload form with a list of already uploaded files.'''
     mypath = 'media'
     allfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     if request.method == 'POST' and request.FILES['upload']:
+        '''handles file uploads and redirects to upload'''
         upload = request.FILES['upload']
         fss = FileSystemStorage()
         file = fss.save(upload.name, upload)
-        file_url = fss.url(file)
-        mypath = 'media'
-        return render(request, 'upload_files/upload.html', {'file_url': file_url, 'allfiles': allfiles})
-    mypath = 'media'
+#        file_url = fss.url(file)
+#        mypath = 'media'
+        return redirect("/upload")
     return render(request, 'upload_files/upload.html', {'allfiles': allfiles})
 
 def file(request):
