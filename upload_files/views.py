5ec3bfbc5e7219ada0c812d9ce8fc7795d035e05
KWunsch <kevin.wunsch@tu-darmstadt.de>
2023-03-15 15:33:35 +0100

    display list of uploaded files on upload.html add logic in views.py to display files on upload.html

--- upload_files/views.py
+++ upload_files/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import FileSystemStorage
+from django.contrib.staticfiles.utils import get_files
+from os.path import isfile, join
+from os import listdir
 
 # Create your views here.
 def index(request):
     return render(request, "upload_files/index.html")
 
 def upload_file(request):
+
+    mypath = 'media'
+    allfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     if request.method == 'POST' and request.FILES['upload']:
         upload = request.FILES['upload']
         fss = FileSystemStorage()
         file = fss.save(upload.name, upload)
         file_url = fss.url(file)
-        return render(request, 'upload_files/upload.html', {'file_url': file_url})
-    return render(request, 'upload_files/upload.html')
+        mypath = 'media'
+        allfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        return render(request, 'upload_files/upload.html', {'file_url': file_url, 'allfiles': allfiles})
+    mypath = 'media'
+    allfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    return render(request, 'upload_files/upload.html', {'allfiles': allfiles})
 
 def file(request):
     return HttpResponse("Starting point for files and details")
